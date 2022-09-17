235348b11ae205bdc35a46ccaa334bed2822dad8
Ahmed Sharabi <ahmeds.alsharabi@gmail.com>
2022-09-17 21:44:30 +0200

    Design changes

--- Book/BookView.swift
+++ Book/BookView.swift
@@ -10,32 +10,32 @@ import SwiftUI
 struct BookView: View {
     @EnvironmentObject var books: Library
     @State private var bookFilter: Library.FilterType = .all
+   
     
     
     let filter: Library.FilterType
     
     var body: some View {
         NavigationStack {
+            
             HStack {
-                Text("Filter by:")
-                    .font(.headline)
-                    .foregroundColor(.secondary)
-                    .padding(.horizontal)
-                
-                
-                
                 Picker("Select", selection: $bookFilter) {
                     Text("All").tag(Library.FilterType.all)
-                    Text("Read").tag(Library.FilterType.read)
                     Text("TBR").tag(Library.FilterType.tbr)
-                    Text("Currently Reading").tag(Library.FilterType.reading)
+                    Text("Reading").tag(Library.FilterType.reading)
+                    Text("Read").tag(Library.FilterType.read)
                 }
+                .pickerStyle(.segmented)
                 .padding(.horizontal)
                 .foregroundColor(.purple)
-
+                
             }
-                List {
-                    ForEach(filteredBooks){ book in
+            
+            List {
+                ForEach(filteredBooks){ book in
+                    NavigationLink {
+                        Text(book.readingState)
+                    } label: {
                         HStack {
                             AsyncImage (url: URL(string: book.info.volumeInfo.imageLinks?.thumbnail ?? "no-image")) { image in
                                 image
@@ -48,20 +48,60 @@ struct BookView: View {
                             
                             VStack(alignment: .leading) {
                                 Text(book.info.volumeInfo.title)
+                                    .font(.caption.bold())
                                 Text(book.info.volumeInfo.authors?[0] ??  "Unknown Author")
-                            }
-                                .multilineTextAlignment(.center)
+                                    .font(.caption)
+                                    .foregroundColor(.secondary)
+                                HStack(alignment: .center) {
+                                    Text("Read")
+                                        .font(.caption.bold())
+                                    Image(systemName: "chevron.down")
+                                        .font(.system(size: 10, weight: .light))
+                                       
+                                        
+                                }
+                                .foregroundColor(.white)
                                 .padding(.horizontal)
-                        }
-                }
-                    .onDelete(perform: books.removeBook)
-                }
-                .navigationTitle("My Books")
-                .navigationBarTitleDisplayMode(.inline)
+                                .padding(.vertical, 5)
+                                .background(.black)
+                                .cornerRadius(10)
+                            }
+                            }
+                            .multilineTextAlignment(.leading)
+                            .padding(.horizontal)
+                            
+                          
+                            
+//                        .contextMenu {
+//                                Button {
+//                                    print(book)
+//                                } label:{
+//                                    Label("Mark as Read", systemImage: "book.closed")
+//                            }
+//                                Button {
+//                                    print(book)
+//                                } label: {
+//                                    Label("Mark as Currently Reading", systemImage: "book")
+//                                }
+//                                Button {
+//                                    print(book)
+//                                } label: {
+//                                    Label("Mark as TBR", systemImage: "bookmark")
+//                                }
 
-            
+//                        }
+
+                    }
+                } .onDelete(perform: books.removeBook)
+                   
             }
+            
+            .navigationTitle("My Books")
+            .navigationBarTitleDisplayMode(.inline)
+            
+            
         }
+    }
     
     var filteredBooks: [SavedBook] {
         switch bookFilter {
@@ -75,4 +115,4 @@ struct BookView: View {
             return books.books.filter{ $0.readingState == "tbr" }
         }
     }
-    }
+}
